3d5930785c9b19705a63db1499a6333368f89bfb
rushingfitness <jordan@rushingfitness.com>
2014-12-20 18:29:25 -0500

    Add further columns to user index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,13 +2,19 @@ class Api::V1::UsersController < ApplicationController
   def index
     begin
       user = User.where(lawyer: true).joins("LEFT JOIN avatars on avatars.user_id = users.id AND avatars.main = true").joins('LEFT JOIN descriptions on descriptions.user_id = users.id').select("users.id, users.first_name, users.middle_name, users.last_name, COALESCE(avatars.img_url,'https://patentmonkapi.herokuapp.com#{asset_path('monk_head_trans.png')}') as img_url, star_count, descriptions.description, bill_per_hour")
+      puts 'user'
       Rails.logger.info 'user'
+      puts user
       Rails.logger.info user
+      puts 'user'
       Rails.logger.info 'user'
     rescue => e
+      puts 'rescue'
       Rails.logger.info 'rescue'
+      puts e
       Rails.logger.info e
       user = []
+      puts 'rescue'
       Rails.logger.info 'rescue'
     end
     respond_to do |format|
