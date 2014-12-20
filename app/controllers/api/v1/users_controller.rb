7efb3577dfefc1faf2eb9e8ffa30cd165499e26a
rushingfitness <jordan@rushingfitness.com>
2014-12-20 16:46:32 -0500

    Add further columns to user index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def index
     respond_to do |format|
-     format.json {
-       render :json => {
-         User.where(lawyer: true)
-                  .joins("LEFT JOIN avatars on avatars.user_id = users.id AND avatars.main = true")
-                  .joins('descriptions on descriptions.user_id = users.id')
-                  .select("users.id, users.first_name, users.middle_name, users.last_name, avatars.img_url, star_count.descriptions.description, bill_per_hour")
-       }
-   end
+     format.json do
+       render :json => { User.where(lawyer: true).joins("LEFT JOIN avatars on avatars.user_id = users.id AND avatars.main = true").joins('descriptions on descriptions.user_id = users.id').select("users.id, users.first_name, users.middle_name, users.last_name, avatars.img_url, star_count.descriptions.description, bill_per_hour") }
+     end
+    end
   end
 
   def show
