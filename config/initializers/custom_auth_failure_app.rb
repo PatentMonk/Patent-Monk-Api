5da59dd49ccb5837b8712c8cd2a096c34b494e8e
rushingfitness <jordan@rushingfitness.com>
2014-12-20 12:18:34 -0500

    See headers

--- config/initializers/custom_auth_failure_app.rb
+++ config/initializers/custom_auth_failure_app.rb
@@ -1,5 +1,10 @@
 class CustomAuthFailure < Devise::FailureApp
   def respond
+    puts "HEADERS"
+    puts request.env.inspect
+    puts "HEADERS"
+    
+    
     self.status = 401 
     self.content_type = 'json'
     self.response_body = {'success' => false,"errors" => ["Invalid login credentials"]}.to_json
