9abf7bae59cb65a2e912aa5417616992d1382fb2
rushingfitness <jordan@rushingfitness.com>
2014-12-20 12:15:29 -0500

    See headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include ActionController::StrongParameters
-
+  puts "HEADERS"
+  puts request.env.inspect
+  puts "HEADERS"
+  
   acts_as_token_authentication_handler_for User
 end
