17791527b399349faa480a64b94d180d48d9a211
rushingfitness <jordan@rushingfitness.com>
2014-12-21 10:54:59 -0500

    Add further columns to user index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def index
     begin
-      user = User.where(lawyer: true).joins("LEFT JOIN avatars on avatars.user_id = users.id AND avatars.main = true").joins('LEFT JOIN descriptions on descriptions.user_id = users.id').select("users.id, users.first_name, users.middle_name, users.last_name, COALESCE(avatars.img_url,'https://patentmonkapi.herokuapp.com#{asset_path('monk_head_trans.png')}') as img_url, star_count, descriptions.description, bill_per_hour")
+      user = User.where(lawyer: true).joins("LEFT JOIN avatars on avatars.user_id = users.id AND avatars.main = true").joins('LEFT JOIN descriptions on descriptions.user_id = users.id').select("users.id, users.first_name, users.middle_name, users.last_name, COALESCE(avatars.img_url,'https://patentmonkapi.herokuapp.com/assets/monk_head_trans.png') as img_url, star_count, descriptions.description, bill_per_hour")
       puts 'user'
       Rails.logger.info 'user'
       puts user
@@ -9,20 +9,8 @@ class Api::V1::UsersController < ApplicationController
       puts 'user'
       Rails.logger.info 'user'
     rescue => e
-      puts 'rescue'
-      Rails.logger.info 'rescue'
-      puts e
-      Rails.logger.info e
-      merp = e
-      derp = user.errors
       user = []
-      puts 'rescue'
-      Rails.logger.info 'rescue'
     end
-    Rails.logger.info 'merp'
-    Rails.logger.info merp
-    Rails.logger.info 'derp'
-    Rails.logger.info derp
     respond_to do |format|
      format.json do
        render json: user
